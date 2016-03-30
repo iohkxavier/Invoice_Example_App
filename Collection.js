0fe59fb6939c0a46cb59241e2f93935297512a41
iohkxavier <xavier.cretin@iohk.io>
2016-03-30 11:17:25 +0900

    remove the end date filter

--- Collection.js
+++ Collection.js
@@ -12,25 +12,25 @@ Invoices.byTimeRange = function(filter, sortBy, sortOrder){
   }
   sortQuery[sortBy] = sortOrder;
 
-  let endDate = new Date();
+  let curDate = new Date();
   let startDate = new Date();
   switch (filter) {
     default:
     case "today":
-      startDate.setDate(endDate.getDate()-1);
+      startDate.setDate(curDate.getDate()-1);
       break;
     case "week":
-      startDate.setDate(endDate.getDate()-7);
+      startDate.setDate(curDate.getDate()-7);
       break;
     case "month":
-      startDate.setMonth(endDate.getMonth()-1);
+      startDate.setMonth(curDate.getMonth()-1);
       break;
     case "all":
       return Invoices.find({}, {sort: sortQuery});
       break;
   }
   return Invoices.find(
-    {createdAt: {$gt: startDate,$lte: endDate}},
+    {createdAt: {$gte: startDate}},
     {sort: sortQuery}
   );
 };
