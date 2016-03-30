7e7ca17dcfca0fc3f996791d77b17c1737027078
iohkxavier <xavier.cretin@iohk.io>
2016-03-30 16:18:50 +0900

    remove session variables aand migrate to flow router instead of iron router

--- Router/Routes.js
+++ Router/Routes.js
@@ -1,49 +1,16 @@
 // given a url like "/week?sortTotal=asc"
-Router.route('/', function () {
-  this.redirect('/today?sortedBy=createdAt&sortOrder=asc');
+FlowRouter.route('/', {
+triggersEnter: [function(context, redirect) {
+        redirect('/today?sortedBy=createdAt&sortOrder=asc');
+    }]
 });
 
-Router.route('/today', {
-  waitOn: function () {
-    // console.log("subscribe today");
-    return Meteor.subscribe('invoices',"today",this.params.query.sortedBy, this.params.query.sortOrder);
-  }
-  , template: 'Invoices',
-  data: function() {
-    //console.log("route / ");
-    return Invoices.byTimeRange("today",this.params.query.sortedBy, this.params.query.sortOrder);
+FlowRouter.route('/:timeRange', {
+    subscriptions: function(params,queryParams) {
+        console.log("subscribe and register this subscription as "+params.timeRange);
+        this.register('Invoices', Meteor.subscribe('invoices',params.timeRange,queryParams.sortedBy, queryParams.sortOrder));
+    },
+    action: function(params,queryParams) {
+         FlowLayout.render('Invoices');
     }
-});
-
-Router.route('/week', {
-    waitOn: function () {
-    //console.log("subscribe week");
-    return Meteor.subscribe("invoices","week",this.params.query.sortedBy, this.params.query.sortOrder);
-  }
-  , template: 'Invoices',
-  data: function() {
-    return Invoices.byTimeRange("week", this.params.query.sortedBy,this.params.query.sortOrder);
-  }
-});
-
-Router.route('/month', {
-    waitOn: function () {
-    //console.log("subscribe week");
-    return Meteor.subscribe("invoices","month",this.params.query.sortedBy, this.params.query.sortOrder);
-  }
-  , template: 'Invoices',
-  data: function() {
-    return Invoices.byTimeRange("month", this.params.query.sortedBy,this.params.query.sortOrder);
-  }
-});
-
-Router.route('/all', {
-    waitOn: function () {
-    //console.log("subscribe week");
-    return Meteor.subscribe("invoices","all",this.params.query.sortedBy, this.params.query.sortOrder);
-  }
-  , template: 'Invoices',
-  data: function() {
-    return Invoices.byTimeRange("all", this.params.query.sortedBy,this.params.query.sortOrder);
-  }
 });
--- client/TimeFilters/Events.js
+++ client/TimeFilters/Events.js
@@ -1,24 +1,24 @@
 //Events: click on Time button filter invoices
 Template.Today.events({
   'click button': function () {
-    Session.set('invoiceTimeRange','today');
-    Router.go("/"+Session.get("invoiceTimeRange")+"?sortedBy="+Session.get("invoiceSortBy")+"&sortOrder="+Session.get("invoiceSortOrder"));
+    GoURL("today");
   }});
 
 Template.Week.events({
   'click button': function () {
-    Session.set('invoiceTimeRange','week');
-    Router.go("/"+Session.get("invoiceTimeRange")+"?sortedBy="+Session.get("invoiceSortBy")+"&sortOrder="+Session.get("invoiceSortOrder"));
+    GoURL("week");
   }});
 
 Template.Month.events({
   'click button': function () {
-    Session.set('invoiceTimeRange','month');
-    Router.go("/"+Session.get("invoiceTimeRange")+"?sortedBy="+Session.get("invoiceSortBy")+"&sortOrder="+Session.get("invoiceSortOrder"));
+    GoURL("month");
   }});
 
 Template.All.events({
   'click button': function () {
-    Session.set('invoiceTimeRange','all');
-    Router.go("/"+Session.get("invoiceTimeRange")+"?sortedBy="+Session.get("invoiceSortBy")+"&sortOrder="+Session.get("invoiceSortOrder"));
+   GoURL("all");
   }});
+
+function GoURL(timeRange) {
+  FlowRouter.setParams({timeRange: timeRange});
+}
