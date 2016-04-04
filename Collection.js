2fc338d1f1a50640499630cbccc9b4e720d9dba4
iohkxavier <xavier.cretin@iohk.io>
2016-04-04 12:08:22 +0900

    limit the query to the apropriate time range

--- Collection.js
+++ Collection.js
@@ -12,26 +12,29 @@ Invoices.byTimeRange = function(filter, sortBy, sortOrder,limit){
   }
   sortQuery[sortBy] = sortOrder;
 
-  let curDate = new Date();
+  let endDate = new Date();
   let startDate = new Date();
+  let daywrapper = moment(startDate);
   switch (filter) {
     default:
     case "today":
-      startDate.setDate(curDate.getDate()-1);
+     daywrapper.startOf('day'); 
       break;
     case "week":
-      startDate.setDate(curDate.getDate()-7);
+      daywrapper.startOf('week');
+      daywrapper.add(1, 'days');//begin on mondays
       break;
     case "month":
-      startDate.setMonth(curDate.getMonth()-1);
+      daywrapper.startOf('month'); 
       break;
     case "all":
-      return Invoices.find({}, {sort: sortQuery,limit:limit});
+      return Invoices.find({}, 
+        {sort: sortQuery,limit:limit});
       break;
   }
   return Invoices.find(
-    {createdAt: {$gte: startDate}},
-    { sort: sortQuery,limit:limit }
+    {createdAt: {$gte: daywrapper.toDate(),$lte:  endDate}},
+    {sort: sortQuery,limit:limit}
   );
 };
 
