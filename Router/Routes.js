6098410faa9ae77eca9f9b800024ea64a780e769
iohkxavier <xavier.cretin@iohk.io>
2016-04-04 12:41:00 +0900

    Subscriptions were duplicated: at template level and at router level

--- Router/Routes.js
+++ Router/Routes.js
@@ -4,9 +4,6 @@ FlowRouter.route('/', {
 });
 
 FlowRouter.route('/:timeRange', {
-  subscriptions: function(params,queryParams) {
-    this.register('Invoices', Meteor.subscribe('invoices',params.timeRange,queryParams.sortedBy, queryParams.sortOrder));
-  },
   action: function(params,queryParams) {
     FlowLayout.render('Invoices');
   }
